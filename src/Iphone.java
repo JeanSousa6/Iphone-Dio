16afbecc13b159bcf5692b234c2d265411f52408
Jean <jean.sousa6@outlook.com>
2024-07-20 13:02:43 -0300

    Adicionando as interfaces

---
+++ src/Iphone.java
@@ -0,0 +1,47 @@
+public class Iphone implements AparelhoTelefonico, NavegadorInternet, ReprodutorMusical{
+
+
+    @Override
+    public void ligar() {
+    }
+
+    @Override
+    public void atender() {
+
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+
+    }
+
+    @Override
+    public void exibirPagina() {
+
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+
+    }
+
+    @Override
+    public void atualizarPagina() {
+
+    }
+
+    @Override
+    public void tocar() {
+
+    }
+
+    @Override
+    public void pausar() {
+
+    }
+
+    @Override
+    public void selecionarMusica() {
+
+    }
+}
